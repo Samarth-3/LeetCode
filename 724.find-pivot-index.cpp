1d4ae64995c9cebb4eea5813a2c0cb9760e111bd
Samarth-3 <spaliwal_be21@thapar.edu>
2023-07-15 23:45:01 +0530

    array

---
+++ 724.find-pivot-index.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=724 lang=cpp
+ *
+ * [724] Find Pivot Index
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int prefix_sum  =0;
+        for(int i= 0; i < nums.size(); i++){
+            prefix_sum += nums[i];
+        }
+        int left_sum =0; 
+        int right_sum = prefix_sum ;
+        for(int i= 0; i < nums.size(); i++){
+            right_sum = right_sum - nums[i];
+            if(left_sum == right_sum)
+                return i;
+            
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
