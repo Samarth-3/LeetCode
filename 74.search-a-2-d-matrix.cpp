4a856f772984902c494d77916155ce0942e4ddbc
Samarth-3 <spaliwal_be21@thapar.edu>
2023-07-17 22:11:38 +0530

    2D Array

---
+++ 74.search-a-2-d-matrix.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=74 lang=cpp
+ *
+ * [74] Search a 2D Matrix
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        int start=0;
+        int end=(row * col )-1;
+        int mid=(start+end)/2;
+        
+        while(start<=end){
+            int element=matrix[mid/col][mid%col];
+            if(target==element){
+                return 1;
+            }
+            else if (target>element){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            mid=(start+end)/2;
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
