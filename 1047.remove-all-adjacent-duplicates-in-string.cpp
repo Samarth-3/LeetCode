08d11809b590e2bba9d2a31d821814b33fbeeabb
Samarth-3 <spaliwal_be21@thapar.edu>
2023-07-20 23:28:34 +0530

    string

---
+++ 1047.remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1047 lang=cpp
+ *
+ * [1047] Remove All Adjacent Duplicates In String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string temp = "";
+        int i = 0;
+        
+        while (i < s.length()) {
+            if (temp.empty() || s[i] != temp.back()) {
+                temp.push_back(s[i]);
+            } else {
+                temp.pop_back();
+            }
+            i++;
+        }
+        
+        return temp;
+    }
+};
+
+// @lc code=end
+
